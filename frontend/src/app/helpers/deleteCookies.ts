62a53e9448e5964aeffd75720cdb78cbb8689500
tomaslb26 <tomaslb26@gmail.com>
2023-11-27 17:53:54 +0000

    Update API endpoint in deleteCookies function

--- frontend/src/app/helpers/deleteCookies.ts
+++ frontend/src/app/helpers/deleteCookies.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 
 export default async function deleteCookies(){
-    await fetch("http://localhost:3000/api/logout", {
+    await fetch("/api/logout", {
         method: "DELETE",
         headers: {
             "Content-Type": "application/json"
