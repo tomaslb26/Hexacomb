dc33be6b2924f283c2b1b329aedd5c9119260755
tomaslb26 <tomaslb26@gmail.com>
2023-11-27 17:26:38 +0000

    Fix server-side rendering issue and handle user authentication

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use server;"
+
 import Hero from "@/components/LandingPage/Hero";
 import Layout from "@/components/Layout/Layout";
 import { cookies } from "next/headers";
@@ -6,33 +8,36 @@ import { redirect } from "next/navigation";
 import deleteCookies from "./helpers/deleteCookies";
 
 async function getData(){
-  "use server;"
-
-  let token = undefined;
-  if(cookies().get("token")){
-    token = cookies().get("token")?.value;
-  }
-
-
-  let user = undefined;
   try{
-    if(token){
-      user = await getUser(token);
+    let token = undefined;
+    if(cookies().get("token")){
+      token = cookies().get("token")?.value;
     }
+
+
+    let user = undefined;
+    try{
+      if(token){
+        user = await getUser(token);
+      }
+    }catch(e){
+      console.log(e);
+      return undefined;
+    }
+
+    if(user && !user.verified){
+      redirect("/2fa");
+    }
+
+    if(user === undefined){
+      deleteCookies();
+      return undefined;
+    }
+    return user;
   }catch(e){
     console.log(e);
     return undefined;
   }
-
-  if(user && !user.verified){
-    redirect("/2fa");
-  }
-
-  if(user === undefined){
-    deleteCookies();
-    return undefined;
-  }
-  return user;
 }
 
 
