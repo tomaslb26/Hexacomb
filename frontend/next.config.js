7db1f2f3386376c31f8492ec830e0af3fe86dfaa
tomaslb26 <tomaslb26@gmail.com>
2023-11-27 20:41:57 +0000

    Add Content-Security-Policy headers to next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -13,6 +13,19 @@ const nextConfig = {
         },
       ],
     },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self';",
+            },
+          ],
+        },
+      ];
+    },
   };
 
 module.exports = nextConfig
